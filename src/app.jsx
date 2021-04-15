7c5f7fb7b0d48b2d1bcc2d8db0106574ab771822
Hong-sunki <tjsrl434@naver.com>
2021-04-15 19:15:29 +0900

    show video list with title

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './app.css';
+import VideoList from './components/video_list/video_list';
 
 function App() {
+  const [videos, setVideos] = useState([]);
+  
+  
+  useEffect(()=>{
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+    
+    fetch("https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResult=25&key=AIzaSyARy32ck1J1QhjEGch0KdESun9hkKaS2UU", requestOptions)
+      .then(response => response.json())
+      .then(result => setVideos(result.items))
+      .catch(error => console.log('error', error));
+  }, []);
   return (
-    <h1>Hello:)</h1>
+    <VideoList videos={videos} />
   );
 }
 
